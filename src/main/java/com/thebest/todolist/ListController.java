4b6115dd4367f7e25769845af135eff4de6b2c6d
Danrold <danrold@mail.ru>
2020-10-28 11:47:32 +0700

    add methods to ListController

--- src/main/java/com/thebest/todolist/ListController.java
+++ src/main/java/com/thebest/todolist/ListController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import java.util.*;
 
 @RestController
 @RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -24,7 +24,7 @@ public class ListController {
 
 
     @PostMapping("/list")
-    public ResponseEntity<List> add(@RequestParam String listName) {
+    public ResponseEntity<UUID> add(@RequestParam String listName) {
         if(listName.isEmpty() || listName == null) {
             return new ResponseEntity("incorrect name", HttpStatus.NOT_ACCEPTABLE);
         }
@@ -33,7 +33,38 @@ public class ListController {
         list.setCreateDate(new Date());
         list.setChangeDate(new Date());
 
+        listRepository.save(list);
+
+        return ResponseEntity.ok(list.getId());
+    }
+
+    @PutMapping("/list")
+    public ResponseEntity<List> updateList(@RequestParam UUID id, String newName)
+    {
+        if(newName.isEmpty() || newName == null) {
+            return new ResponseEntity("incorrect name", HttpStatus.NOT_ACCEPTABLE);
+        }
+        List list = listRepository.getOne(id);
+        if (list == null){
+            return new ResponseEntity("incorrect id", HttpStatus.NOT_FOUND);
+        }
+
+        list.setListName(newName);
+        list.setChangeDate(new Date());
+
         return ResponseEntity.ok(listRepository.save(list));
+    }
+
+    @DeleteMapping("/list")
+    public ResponseEntity deleteList(@RequestParam UUID id)
+    {
+        List list = listRepository.getOne(id);
+        if (list == null){
+            return new ResponseEntity("incorrect id", HttpStatus.NOT_FOUND);
+        }
+        listRepository.delete(list);
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
